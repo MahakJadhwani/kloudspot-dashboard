dc5c25c4ad3c7ffb991331f5b35ff8030ede0c2b
MahakJadhwani <jadhwanimahak@gmail.com>
2025-12-17 01:38:33 +0530

    Remove nested BrowserRouter to fix blank screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,38 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/TempLogin";
 import Dashboard from "./pages/TempDashboard";
 import CrowdEntries from "./pages/CrowdEntries";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-
-
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Public Route */}
-        <Route path="/login" element={<Login />} />
+    <Routes>
+      {/* Public Route */}
+      <Route path="/login" element={<Login />} />
 
-        {/* Protected Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* Protected Routes */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/crowd-entries"
+        element={
+          <ProtectedRoute>
+            <CrowdEntries />
+          </ProtectedRoute>
+        }
+      />
 
-        <Route
-          path="/crowd-entries"
-          element={
-            <ProtectedRoute>
-              <CrowdEntries />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Fallback route: redirect unknown paths to login */}
-        <Route path="*" element={<Login />} />
-
-        <Route
-          path="/crowd-entries"
-          element={
-            <ProtectedRoute>
-              <CrowdEntries />
-            </ProtectedRoute>
-          }
-        />
-
-      </Routes>
-    </BrowserRouter>
+      {/* Fallback route: redirect unknown paths to login */}
+      <Route path="*" element={<Login />} />
+    </Routes>
   );
 }
 
 export default App;
+
